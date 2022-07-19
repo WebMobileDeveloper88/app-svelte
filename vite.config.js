602066196d5d6414a5d862cf0f9187c7e1f9d46e
WebMobileDeveloper88 <web_mobile_developers@gmx.eu>
2022-07-19 14:35:49 +0200

    server attribute in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    host: true,
-    port: 8080
-  }
+     server: {
+       host: true,
+       port: 5000
+  }  
 })
 
 
